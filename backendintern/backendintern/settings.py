384505d3274b60a4755ab73bd353e968262335b0
naveen <naveenpoolakuntla09@gmail.com>
2025-08-26 17:04:45 +0530

    internship

--- backendintern/backendintern/settings.py
+++ backendintern/backendintern/settings.py
@@ -80,12 +80,25 @@ WSGI_APPLICATION = 'backendintern.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'internship',       
+        'HOST': '150.241.246.242', 
+        'USER': 'root', 
+        'PASSWORD': 'Tsarit@12345',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
